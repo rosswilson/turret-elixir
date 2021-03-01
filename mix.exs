751c24b9c0517a6892e3d52c5dce27d368b7c075
Ross Wilson <ross@tixil.com>
2021-03-01 10:55:06 +0000

    Upgraded to Phoenix 1.5.8

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Turret.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 2.0"},
-      {:phoenix, "~> 1.5.7"},
+      {:phoenix, "~> 1.5.8"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
